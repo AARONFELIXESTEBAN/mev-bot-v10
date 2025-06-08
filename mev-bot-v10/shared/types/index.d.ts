2ca02ce3845d7dd7d62a81a5c605ac12d2389e82
SHENINDRIVER <aaronfelixesteban@gmail.com>
2025-06-09 04:50:59 +0800

    fix SimulationService

---
+++ mev-bot-v10/shared/types/index.d.ts
@@ -0,0 +1,25 @@
+import { ethers, utils as ethersUtils, providers } from 'ethers';
+export interface DecodedTransactionInput {
+    functionName: string;
+    signature: string;
+    args: ethersUtils.Result;
+    path?: string[];
+    amountOutMin?: ethers.BigNumber;
+    amountIn?: ethers.BigNumber;
+    amountOut?: ethers.BigNumber;
+    amountInMax?: ethers.BigNumber;
+    to?: string;
+    deadline?: ethers.BigNumber;
+    tokenA?: string;
+    tokenB?: string;
+    amountADesired?: ethers.BigNumber;
+    amountBDesired?: ethers.BigNumber;
+    amountAMin?: ethers.BigNumber;
+    amountBMin?: ethers.BigNumber;
+    liquidity?: ethers.BigNumber;
+}
+export interface FilterableTransaction extends providers.TransactionResponse {
+    decodedInput?: DecodedTransactionInput & {
+        routerName: string;
+    };
+}
---
+++ mev-bot-v10/shared/types/index.js
@@ -0,0 +1,3 @@
+"use strict";
+// In shared/types/index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
